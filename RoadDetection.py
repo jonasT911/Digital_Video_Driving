24aec27139d3c1369fac7d9a3a82ac174c2ba872
Jonas Traweek <jonastraweek@yahoo.com>
2023-03-28 22:24:16 -0500

    Beginning Of Road Detection

---
+++ RoadDetection.py
@@ -0,0 +1,14 @@
+
+class roadDetector:
+
+    color= [0,0,0]
+    
+    def findRoad( roadColor, picture):
+        
+        pass
+        
+    def setInitialRoadColor(picture, location):
+        pass
+        
+    def importPhoto(pictureName)#For testing purposes
+        pass
