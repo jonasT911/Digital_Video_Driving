a0ffde80067398bc474edeb48726606a347b4362
Jonas Traweek <jonastraweek@yahoo.com>
2023-04-06 11:06:15 -0500

    Get road color function added

--- RoadDetection.py
+++ RoadDetection.py
@@ -1,14 +1,36 @@
+import cv2
+import numpy as np
 
-class roadDetector:
-
-    color= [0,0,0]
+def colorDifferenceRGB(colorOne,colorTwo):
+    pass
     
-    def findRoad( roadColor, picture):
+def luminenceDifference(colorOne,colorTwo):
+    pass
+    
+def findRoad( roadColor, picture):
+    
+    pass
+    
+def updateRoadColor(picture):
+    width=picture.shape[1]
+    height=picture.shape[0]
+    cropped=picture[(height-100):height-50 ,int(width/4):int( 3*width/4), :]
+    divided=cropped/(cropped.shape[0]*cropped.shape[1])
+    #cv2.imwrite("crop.png", cropped) 
+    avg=sum(sum(divided))
+    output=np.array(avg, dtype=np.uint8)
+
+    return output
+    
+def importPhoto(pictureName):#For testing purposes
+    img = cv2.imread(pictureName, cv2.IMREAD_ANYCOLOR)
+    
         
-        pass
-        
-    def setInitialRoadColor(picture, location):
-        pass
-        
-    def importPhoto(pictureName)#For testing purposes
-        pass
+if __name__== '__main__':
+    #Run testing Script
+
+    img = cv2.imread("FlatTrackDrive/test_1.png", cv2.IMREAD_ANYCOLOR)
+    
+    color = updateRoadColor(img)
+    print("color is " +str(color))
+    
